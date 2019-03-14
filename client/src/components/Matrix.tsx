3025522133d1e2d7e091df06acee913f3e1910b5
igorrmotta <igor@edapp.com>
2019-03-14 22:14:31 +1100

    👾Create mock of grid

---
+++ client/src/components/Matrix.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './Matrix.sass';
+
+const Matrix: React.FunctionComponent<{}> = () => {
+  return <div className="matrix-container" />;
+};
+
+export { Matrix };
